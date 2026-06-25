c74e001ad199eeccf5d864d5c2a6e592644c54ad
atahancelenk <celenksinasiatahan@gmail.com>
2026-06-25 12:19:33 +0200

    Refactor: Remove unnecessary custom headers from ETL pipeline

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -4,21 +4,6 @@ from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
 import time
 
-# Make the cloud server appear like a normal browser
-custom_headers = {
-    'Host': 'stats.nba.com',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
-    'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'x-nba-stats-origin': 'stats',
-    'x-nba-stats-token': 'true',
-    'Connection': 'keep-alive',
-    'Referer': 'https://stats.nba.com/',
-    'Pragma': 'no-cache',
-    'Cache-Control': 'no-cache',
-}
-
 player_names = ["LeBron James", "Russell Westbrook", "Stephen Curry"]
 
 all_stats = pd.DataFrame()
